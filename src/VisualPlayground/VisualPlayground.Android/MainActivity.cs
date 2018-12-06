29705c70d351a437d193c23a04f5aa8a497a5852
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-12-06 19:24:21 +0100

    Initial Commit

---
+++ src/VisualPlayground/VisualPlayground.Android/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Xamarin.Forms;
+
+namespace VisualPlayground.Droid
+{
+    [Activity(
+        Label = "VisualPlayground", 
+        Icon = "@mipmap/icon", 
+        Theme = "@style/MainTheme", 
+        MainLauncher = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            Forms.SetFlags("Visual_Experimental");
+            Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
